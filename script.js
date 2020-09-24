3858f0c0b0c7af6c56c22ba79576e9e0efb21c82
Rafael dos Santos Galdino Soares <rafael.soares@rebic.com.br>
2020-09-24 17:27:42 -0300

    iniciando com Java_Script

---
+++ script.js
@@ -0,0 +1,173 @@
+let cart = [];
+let modalQt = 1;
+let modalKey = 0;
+
+// Função de Selecionar
+const c  = (el) =>document.querySelector(el)
+const cs = (el) =>document.querySelectorAll(el)
+
+
+// Função chamando modelo das Pizzas e mostrar na tela(Listagem)
+pizzaJson.map((item, index)=> {
+    const pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+      
+    pizzaItem.setAttribute('data-key', index)
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    // Função Mostrando o Modal por Evento de Click
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        const key = e.target.closest('.pizza-item').getAttribute('data-key')
+        modalQt = 1;
+        modalKey = key;
+
+        c('.pizzaBig img').src = pizzaJson[key].img
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+        // Mostra o tamanho da pizza e fixar pelo maior tamanho
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+            if (sizeIndex == 2){
+                    size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+        // Adicionando Quantidade de Pizza no Modal
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+
+        c('.pizzaWindowArea').style.opacity = 0
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1
+        }, 200)        
+    })
+    // Preencher as informações em pizzaItem
+        c('.pizza-area').append(pizzaItem)
+})
+
+// Eventos do Modal
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) =>{
+    item.addEventListener('click', closeModal)
+})
+// Adicionando função de menos e mais "Button"
+c('.pizzaInfo--qtmenos').addEventListener('click', () =>{
+    if (modalQt > 1) {
+        modalQt--
+    }
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+c('.pizzaInfo--qtmais').addEventListener('click', () =>{
+  modalQt++
+  c('.pizzaInfo--qt').innerHTML = modalQt;
+})
+
+// Açoes de Click Modal seleção de tamanhos de pizza
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+});
+
+// Ação para adicionar pizza ao Carrinho
+c('.pizzaInfo--addButton').addEventListener('click', () =>{   
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
+    // Criando identificador 
+    let identificador = pizzaJson[modalKey].id+'@'+size
+    // Verifica no carrinho se ja tem o mesmo identificador que foi criado  
+    let key = cart.findIndex((item) =>item.identificador == identificador)
+    if(key > - 1){
+        cart[key].qt +=modalQt
+    }else{
+    cart.push({
+        identificador,
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+    })
+}
+    updateCart();
+    closeModal();
+});
+    // Função mostra o carrinho Mobile
+    c('.menu-openner').addEventListener('click', ()=>{
+        if (cart.length > 0){
+            c('aside').style.left = '0'    
+        }    
+    }) 
+    c('.menu-closer').addEventListener('click', ()=>{
+        c('aside').style.left = '100vw'
+    })
+
+    // Mostrar Carrinho de Compras e atualizar
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length
+
+    if(cart.length > 0){
+        c('aside').classList.add('show')
+        c('.cart').innerHTML = ''
+
+        let subTotal = 0
+        let desconto = 0
+        let total = 0
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subTotal += pizzaItem.price * cart[i].qt
+
+            let cartItem = c('.models .cart--item').cloneNode(true)
+
+            let pizzaSizeName
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = 'P'
+                    break
+                case 1:
+                    pizzaSizeName = 'M'
+                    break       
+                case 2:
+                    pizzaSizeName = 'G'
+                    break    
+            }
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+            cartItem.querySelector('img').src = pizzaItem.img
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--    
+                } else {
+                    cart.splice(i, 1)
+                }
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++
+                updateCart()
+            })
+            c('.cart').append(cartItem)
+
+        }
+        desconto = subTotal * 0.1
+        total = subTotal - desconto
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
+
+    } else {
+        c('aside').classList.remove('show')
+        c('aside').style.left = '100vw'
+    }               
+}
